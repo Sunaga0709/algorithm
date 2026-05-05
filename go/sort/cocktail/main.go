f6a090b9fa000548cc13aa3c968c68652ae2e669
Takashi Sunaga <sunaga@TakashinoMacBook-Pro.local>
2026-05-05 10:10:02 +0900

    cocktail

--- go/sort/cocktail/main.go
+++ go/sort/cocktail/main.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	cocktailSort([]int{5, 7, 1, 34, 22, 59, 18, 9, 2})
+}
+
+func cocktailSort(numbers []int) {
+	length := len(numbers)
+	swap := true
+	var start int
+	end := length - 1
+	var cnt int
+	for {
+		swap = false
+		fmt.Println("~ backward ~")
+		for i := start; i < end; i++ {
+			if numbers[i] > numbers[i+1] {
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+				swap = true
+			}
+			fmt.Printf("%3d_%d_%d ... %v\n", cnt, start, end, numbers)
+		}
+
+		if !swap {
+			break
+		}
+
+		swap = false
+		end--
+
+		fmt.Println("~ forward ~")
+		for i := end - 1; i > start-1; i-- {
+			if numbers[i] > numbers[i+1] {
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+				swap = true
+			}
+			fmt.Printf("%3d_%d_%d ... %v\n", cnt, start, end, numbers)
+		}
+
+		start++
+		cnt++
+	}
+}

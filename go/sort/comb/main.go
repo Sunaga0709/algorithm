df6cc72c8ad1ecaf7ce87a39b546617df9f99e15
Takashi Sunaga <sunaga@M4MacBookAir.local>
2026-05-14 02:19:39 +0900

    comb sort

--- go/sort/comb/main.go
+++ go/sort/comb/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	combSort([]int{5, 6, 4, 9, 13, 39})
+}
+
+func combSort(numbers []int) {
+	numbersLen := len(numbers)
+	gap := numbersLen
+	swapped := true
+
+	var cnt int
+	for gap > 1 || swapped {
+		cnt++
+
+		gap = int(float64(gap) / 1.3)
+		if gap < 1 {
+			gap = 1
+		}
+
+		swapped = false
+		for i := 0; i < numbersLen-gap; i++ {
+			if numbers[i] > numbers[i+gap] {
+				numbers[i], numbers[i+gap] = numbers[i+gap], numbers[i]
+				swapped = true
+			}
+			fmt.Printf("%3d_%d gap: %d, swapped: %v, numbers: %v\n", cnt, i, gap, swapped, numbers)
+
+		}
+	}
+}

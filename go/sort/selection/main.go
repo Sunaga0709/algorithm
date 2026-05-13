15b69ba8f9351b11d3016018f8c7c054360718de
Takashi Sunaga <sunaga@TakashinoMacBook-Pro.local>
2026-05-14 02:35:19 +0900

    selection sort

--- go/sort/selection/main.go
+++ go/sort/selection/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	selectionSort([]int{5, 6, 4, 9, 13, 39, 1})
+}
+
+func selectionSort(numbers []int) {
+	numbersLen := len(numbers)
+	for i := range numbersLen {
+		minIndex := i
+		for j := i + 1; j < numbersLen; j++ {
+			if numbers[minIndex] > numbers[j] {
+				minIndex = j
+			}
+		}
+
+		numbers[i], numbers[minIndex] = numbers[minIndex], numbers[i]
+
+		fmt.Printf("%3d numbers: %+v\n", i, numbers)
+	}
+}

8fcd83af0d3717f2b5ed87f73946eebbe2e4f80f
Takashi Sunaga <sunaga@TakashinoMacBook-Pro.local>
2026-05-25 12:37:54 +0900

    gnome

--- go/sort/gnome/main.go
+++ go/sort/gnome/main.go
@@ -1,10 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	gnomeSort([]int{5, 7, 1, 4, 2, 9, 8, 9, 2})
 }
 
 func gnomeSort(numbers []int) {
 	var index int
-	maxIndex := len(numbers) - 1
+	for index < len(numbers) {
+		if index == 0 || numbers[index-1] <= numbers[index] {
+			index += 1
+		} else {
+			numbers[index-1], numbers[index] = numbers[index], numbers[index-1]
+			index -= 1
+		}
+		fmt.Printf("%2d: %+v\n", index, numbers)
+	}
+
+	fmt.Println("---")
+	fmt.Printf("result: %+v\n", numbers)
 }

d24286e7855982c8e8b195eebf82d81430a102ad
Takashi Sunaga <sunaga@M2Pro-MacBookPro.local>
2026-05-31 03:19:26 +0900

    shell sort

--- go/sort/shell/main.go
+++ go/sort/shell/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	shellSort([]int{51, 47, 11, 34, 102, 92, 84, 29, 52, 43})
+}
+
+func shellSort(numbers []int) {
+	length := len(numbers)
+	gap := length / 2
+
+	for gap > 0 {
+		fmt.Printf("--- gap: %d ---\n", gap)
+		for i := gap; i < length; i++ {
+			temp := numbers[i]
+			j := i
+			for j >= gap && numbers[j-gap] > temp {
+				numbers[j] = numbers[j-gap]
+				j -= gap
+			}
+
+			numbers[j] = temp
+			fmt.Printf("temp: %3d, numbers: %+v\n", temp, numbers)
+		}
+
+		gap /= 2
+	}
+
+	fmt.Printf("---\nnumbers: %+v\n", numbers)
+}
